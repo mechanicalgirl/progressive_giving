7684f69d8d468d7eedf628f34b8ba83a1de31761
mechanicalgirl <barbara.shaurette@gmail.com>
2016-12-16 13:54:12 -0600

    adjust tweet text in the admin form

--- giving/recipients/admin.py
+++ giving/recipients/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 
 from .models import Category, Recipient
+from .forms import RecipientModelForm
 
 class RecipientAdmin(admin.ModelAdmin):
     list_display = ('name', 'url', 'category', 'last_posted_date', 'active')
     ordering = ('-last_posted_date',)
     search_fields = ('name',)
+    form = RecipientModelForm
 
 admin.site.register(Category)
 admin.site.register(Recipient, RecipientAdmin)
---
+++ giving/recipients/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+
+from .models import Recipient
+
+class RecipientModelForm( forms.ModelForm ):
+    tweet_text = forms.CharField(widget=forms.Textarea, required=False)
+
+    class Meta:
+        model = Recipient
+        fields = ['category', 'name', 'url', 'twitter_handle', 'tweet_text', 'last_posted_date', 'active', 'newsletter_text', 'newsletter_sent_date', 'newsletter_active']
