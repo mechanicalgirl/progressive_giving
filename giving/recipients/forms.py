c22652b437ce04fdd61153cd469636b87a26da01
mechanicalgirl <barbara.shaurette@gmail.com>
2017-01-08 16:55:00 -0600

    adjust recipient change page

--- giving/recipients/forms.py
+++ giving/recipients/forms.py
@@ -7,4 +7,4 @@ class RecipientModelForm( forms.ModelForm ):
 
     class Meta:
         model = Recipient
-        fields = ['active', 'can_donate_to', 'category', 'name', 'name_slug', 'url', 'twitter_handle', 'tweet_text', 'last_posted_date', 'facebook_url', 'newsletter_text', 'newsletter_sent_date', 'newsletter_active']
+        fields = ['active', 'can_donate_to', 'category', 'name', 'name_slug', 'url', 'twitter_handle', 'tweet_text', 'facebook_url', 'last_posted_date', 'newsletter_text', 'newsletter_sent_date', 'newsletter_active']
