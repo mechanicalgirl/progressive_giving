02c964d1535de3ca6eacd6cd6c40197149e49c2d
mechanicalgirl <barbara.shaurette@gmail.com>
2017-02-11 13:48:44 -0600

    admin change

--- giving/recipients/admin.py
+++ giving/recipients/admin.py
@@ -5,7 +5,7 @@ from .models import Category, Recipient
 from .forms import RecipientModelForm
 
 class RecipientAdmin(admin.ModelAdmin):
-    list_display = ('name', 'category', 'last_posted_date', 'active', 'can_donate_to', 'is_tw_text', 'is_fb_text', 'newsletter_active')
+    list_display = ('name', 'category', 'last_posted_date', 'active', 'can_donate_to', 'is_tw_text', 'is_fb_text', 'is_news_active')
     list_filter = ('category', )
     ordering = ('-last_posted_date',)
     search_fields = ('name',)
@@ -27,6 +27,14 @@ class RecipientAdmin(admin.ModelAdmin):
         return format_html('<img src="/static/admin/img/icon-{}.svg" alt="True" data-pin-nopin="true">', color)
     is_fb_text.short_description = 'FB'
     is_fb_text.admin_order_field = 'facebook_text'
+
+    def is_news_active(self, obj):
+        color = 'no'
+        if obj.newsletter_active:
+            color = 'yes'
+        return format_html('<img src="/static/admin/img/icon-{}.svg" alt="True" data-pin-nopin="true">', color)
+    is_news_active.short_description = 'NW'
+    is_news_active.admin_order_field = 'newsletter_active'
 
 admin.site.register(Category)
 admin.site.register(Recipient, RecipientAdmin)
