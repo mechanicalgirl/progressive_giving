72746d391f395af0f3e2af270cb4769f1c32fe12
mechanicalgirl <barbara.shaurette@gmail.com>
2017-02-12 16:30:38 -0600

    admin changes

--- giving/recipients/admin.py
+++ giving/recipients/admin.py
@@ -5,7 +5,7 @@ from .models import Category, Recipient
 from .forms import RecipientModelForm
 
 class RecipientAdmin(admin.ModelAdmin):
-    list_display = ('name', 'category', 'last_posted_date', 'active', 'can_donate_to', 'is_tw_text', 'is_fb_text', 'is_news_active')
+    list_display = ('name', 'category', 'last_posted_date', 'active', 'can_donate_to', 'is_tw_text', 'is_fb_text', 'has_fb_url', 'is_news_active')
     list_filter = ('category', )
     ordering = ('-last_posted_date',)
     search_fields = ('name',)
@@ -19,6 +19,14 @@ class RecipientAdmin(admin.ModelAdmin):
         return format_html('<img src="/static/admin/img/icon-{}.svg" alt="True" data-pin-nopin="true">', color)
     is_tw_text.short_description = 'TW'
     is_tw_text.admin_order_field = 'tweet_text'
+
+    def has_fb_url(self, obj):
+        color = 'no'
+        if obj.facebook_url:
+            color = 'yes'
+        return format_html('<img src="/static/admin/img/icon-{}.svg" alt="True" data-pin-nopin="true">', color)
+    has_fb_url.short_description = 'FBU'
+    has_fb_url.admin_order_field = 'facebook_url'
 
     def is_fb_text(self, obj):
         color = 'no'
