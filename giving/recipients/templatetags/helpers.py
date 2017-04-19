3806f28f7a082ba72d79ef56daee085c61dc8c25
Andrew Dupont <github@andrewdupont.net>
2017-04-19 17:01:32 -0500

    Hyphenate category titles better. This is pretty silly.

--- giving/recipients/templatetags/helpers.py
+++ giving/recipients/templatetags/helpers.py
@@ -1,6 +1,12 @@
 from django import template
 from django.utils.html import format_html
 
+import re
+import pyphen
+
+pyphen.language_fallback('en_US')
+dic = pyphen.Pyphen(lang='en_US')
+
 register = template.Library()
 
 @register.simple_tag
@@ -19,3 +25,21 @@ def link_to_recipient(r, classname=""):
 
   out += format_html(u"</span>")
   return out
+  
+
+@register.simple_tag
+def mark_for_hyphenation(text):
+  words = text.split(' ')
+  hyphenated_words = [choose_hyphens(word) for word in words]
+  return format_html( ' '.join(hyphenated_words) )
+  
+def choose_hyphens(word):
+  hyph = dic.inserted(word, '-')
+  # Remove the last hyphen unless there are at least 4 letters after it.
+  # Otherwise we get stuff like:
+  #       humanitari-
+  #       an
+  if (len(word) > 6) and (not re.search('-\\w{4,}$', hyph)):
+    hyph = re.sub('-(\\w+)$', '\\1', hyph)
+  
+  return re.sub('-', '&shy;', hyph)
